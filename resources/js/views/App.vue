373222b517cdb7c31211897e7c2885fa03836d3b
Zanco1993 <zanconatofabio@gmail.com>
2022-03-13 21:20:39 +0100

    esercizio completato

---
+++ resources/js/views/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Prima pagina di Vue</h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+
+};
+</script>
+
+<style></style>
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
+Route::get('/home', 'HomeController@index');
 
 Auth::routes();
 
-Route::get('/admin', 'Admin\HomeController@index')->name('admin.home');
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function () {
+        Route::get('/', 'HomeController@index')->name('home');
+    
+    });
+
+Route::get("{any}", function() {
+    return view('home');
+})->where("any", ".*");
