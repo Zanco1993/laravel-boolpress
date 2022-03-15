a56eed51cef96f39e779928d9e3df02247e3864c
Zanco1993 <zanconatofabio@gmail.com>
2022-03-15 16:03:29 +0100

    create table categories

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -2,27 +2,33 @@
 
 @section('content')
 <div class="container">
+  <div class="row justify-content-center">
 
-  <table class="table">
-    @foreach ($users as $user)
+  <div class="col-8">
+
+    
+    <table class="table">
+      @foreach ($users as $user)
       <thead class="thead-dark">
         
-      <tr>
-        <th scope="col">id</th>
-        <th scope="col">Name</th>
-        <th scope="col">e-mail</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th scope="row">{{ $user->id }}</th>
-        <td>{{ $user->name }}</td>
-        <td>{{ $user->email }}</td>
-        
-      </tr>
-    </tbody>
-  @endforeach
-</table>
+        <tr>
+          <th scope="col">id</th>
+          <th scope="col">Name</th>
+          <th scope="col">e-mail</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">{{ $user->id }}</th>
+          <td>{{ $user->name }}</td>
+          <td>{{ $user->email }}</td>
+          
+        </tr>
+      </tbody>
+      @endforeach
+    </table>
+  </div>
+  </div>
   
  
 </div>
